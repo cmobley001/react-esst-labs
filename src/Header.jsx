1bffd64812d2e6d47f6f7c002616ece1757cf818
cmobley001 <cmobley001@student.butte.edu>
2026-04-27 05:54:40 -0700

    reduce image size to BG header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,16 @@
-import heroBg from './img/anime_city-wallpaper-2560x1600.jpg';
+import heroBg from './img/anime_city-wallpaper-1920.jpg';
 
 const Header = ({ title }) => {
   return (
     <header
-      className="relative w-full h-[40vh] flex items-center justify-center bg-center bg-cover bg-no-repeat"
+      className='relative w-full h-[40vh] flex items-center justify-center bg-center bg-cover bg-no-repeat'
       style={{ backgroundImage: `url(${heroBg})` }}
     >
-      <div className="absolute inset-0 bg-black/50" />
-      <div className="relative z-10 text-center px-4">
-        <h1 className="text-5xl font-bold text-white tracking-tight drop-shadow-lg">{title}</h1>
+      <div className='absolute inset-0 bg-black/50' />
+      <div className='relative z-10 text-center px-4'>
+        <h1 className='text-5xl font-bold text-white tracking-tight drop-shadow-lg'>
+          {title}
+        </h1>
       </div>
     </header>
   );
